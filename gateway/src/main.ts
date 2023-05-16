bc7111fa331025cf9612f964aa586c4cdc2ded8d
Marco de Jesus António <md131@hdm-stuttgart.de>
2023-05-16 16:44:32 +0200

    implemented basic eventhandling and text display

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,17 +1,35 @@
-import { Iotee, LogLevel } from "@iotee/node-iotee";
+import { Iotee, LogLevel, ReceiveEvents } from "@iotee/node-iotee";
 import { config } from "dotenv";
 
 // Read .env variables
 config()
 
 const main = async () => {
+  console.log(process.env.DEVICE_URL);
   const iotee = new Iotee(process.env.DEVICE_URL!);
+  let counter = 50;
 
   iotee.setLogLevel(LogLevel.WARN); // Set the verbosity of the API
 
   await iotee.connect();
 
   // Do your commands here
+  iotee.on(ReceiveEvents.ButtonPressed, async (payload) =>{
+      console.log("Button pressed:", payload)
+      switch(payload[0]){
+      case 'A':
+      counter++; break;
+      case 'B':
+      counter++; break;
+      case 'X':
+      counter--; break;
+      case 'Y':
+      counter--; break;
+      default:
+      console.log("failed")}
+  await iotee.setDisplay("current light level: " + counter.toString());
+  }
+    );
 };
 
 main()
