ddcc8947b5691cb55918a281b705fbf19ed775eb
Michael Cabanis <mc071@hdm-stuttgart.de>
2023-06-06 17:34:09 +0200

    chore: add remote store

--- providers.tf
+++ providers.tf
@@ -1,8 +1,10 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
+  backend "http" {
+    required_providers {
+      aws = {
+        source  = "hashicorp/aws"
+        version = "~> 4.0"
+      }
+    } 
   }
 }
