36ea81923f2cd687456ca8a1fee21abfdca40413
ck188 <ck188@hdm-stuttgart.de>
2023-06-23 11:42:22 +0200

    added mqtt publish to light sensor

--- gateway/src/services/LightSensorService.ts
+++ gateway/src/services/LightSensorService.ts
@@ -1,24 +1,31 @@
-import { Iotee, LogLevel, ReceiveEvents } from "@iotee/node-iotee";
+import { Iotee } from "@iotee/node-iotee";
 import { MqttService } from "./mqtt";
 
 export interface LightSensorConfig {
-    iotee: Iotee,
-    mqttService: MqttService,
-    deviceID: string
+  iotee: Iotee;
+  mqttService: MqttService;
+  deviceID: string;
 }
 
 export class LightSensorService {
-    constructor(private config: LightSensorConfig) {
-
+  constructor(private config: LightSensorConfig) {
     const run = async () => {
-        const result = await this.config.iotee.getLight()
-    
-        console.log("result:", result);
-    
-        await this.config.iotee.setDisplay("current light level: \n" + result.toString());
-      }
-    
-      setInterval(run, 1000);
-}
+      const result = await this.config.iotee.getLight();
 
+      console.log("result:", result);
+
+      await this.config.iotee.setDisplay(
+        "current light level: \n" + result.toString()
+      );
+
+      const topic = "thing/light-sensor/" + this.config.deviceID;
+      const message = JSON.stringify({
+        lightLevel: result,
+      });
+
+      this.config.mqttService.publish(topic, message);
+    };
+
+    setInterval(run, 1000);
+  }
 }
