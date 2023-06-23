e131e91b329d9a0a072a83c77d9b7c9b409f3cd8
Marvin Pfau <marvin.pfau@stihl.de>
2023-06-23 12:35:41 +0200

    Add Functions to change Light Level manually.

--- gateway/src/services/LightSensorService.ts
+++ gateway/src/services/LightSensorService.ts
@@ -1,31 +1,79 @@
-import { Iotee } from "@iotee/node-iotee";
+import { Iotee, ReceiveEvents } from "@iotee/node-iotee";
 import { MqttService } from "./mqtt";
 
 export interface LightSensorConfig {
-  iotee: Iotee;
-  mqttService: MqttService;
-  deviceID: string;
+    iotee: Iotee;
+    mqttService: MqttService;
+    deviceID: string;
 }
 
+export enum Mode {
+    AUTO,
+    MANUAL,
+}
+
+const ModeDisplay: { [key in Mode]: string } = {
+    [Mode.AUTO]: "AUTO",
+    [Mode.MANUAL]: "MANUAL",
+  };
+
 export class LightSensorService {
-  constructor(private config: LightSensorConfig) {
-    const run = async () => {
-      const result = await this.config.iotee.getLight();
+    private mode: Mode;
+    private currentLightLevel: number;
 
-      console.log("result:", result);
+    constructor(private config: LightSensorConfig) {
+        this.mode = Mode.AUTO;
+        this.currentLightLevel = 0;
 
-      await this.config.iotee.setDisplay(
-        "current light level: \n" + result.toString()
-      );
+        const run = async () => {
+            if (this.mode === Mode.AUTO) {
+                this.currentLightLevel = await this.config.iotee.getLight();
+            }
 
-      const topic = "thing/light-sensor/" + this.config.deviceID;
-      const message = JSON.stringify({
-        lightLevel: result,
-      });
+            console.log("result:", this.currentLightLevel.toFixed(2));
 
-      this.config.mqttService.publish(topic, message);
-    };
+            await this.config.iotee.setDisplay(
+                "Light Level: \n" + this.currentLightLevel.toFixed(2) + "\n" + "Mode: " + ModeDisplay[this.mode] + "\n" + "A: switch mode \n" + "X: increase light level \n" + "Y: decrease light level"
+            );
 
-    setInterval(run, 1000);
-  }
+            const topic = "thing/light-sensor/" + this.config.deviceID;
+            const message = JSON.stringify({
+                lightLevel: this.currentLightLevel.toFixed(2),
+            });
+
+            this.config.mqttService.publish(topic, message);
+        };
+
+        setInterval(run, 1000);
+
+        this.config.iotee.on(ReceiveEvents.ButtonPressed, async (payload) => {
+            console.log("Button pressed:", payload)
+            switch (payload[0]) {
+                case 'A':
+                    if (this.mode === Mode.AUTO) {
+                        this.mode = Mode.MANUAL;
+                        console.log("MANUAL MODE")
+                    } else if (this.mode === Mode.MANUAL) {
+                        this.mode = Mode.AUTO;
+                        console.log("AUTO MODE")
+                    }; break;
+                case 'X':
+                    if (this.mode === Mode.MANUAL) {
+                        this.currentLightLevel += 50;
+                    }; break;
+                case 'Y':
+                    if (this.mode === Mode.MANUAL) {
+                        this.currentLightLevel -= 50;
+                    }; break;
+                default:
+                    console.log("No Function")
+            }
+        }
+        );
+    }
+
+
+
+
+
 }
