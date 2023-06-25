847b149134941ebfa4db65d026136233b6d951ec
ck188 <ck188@hdm-stuttgart.de>
2023-06-25 13:21:36 +0200

    added mqtt host and port to .env

--- gateway/environment.d.ts
+++ gateway/environment.d.ts
@@ -1,17 +1,19 @@
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            DEVICE_URL: string;
-            DEVICE_TYPE: string;
-            DEVICE_ID: string;
-            SENSOR_INTERVAL: string;
-            CA_PATH: string;
-            CERT_PATH: string;
-            KEY_PATH: string;
-        }
+  namespace NodeJS {
+    interface ProcessEnv {
+      DEVICE_URL: string;
+      DEVICE_TYPE: string;
+      DEVICE_ID: string;
+      SENSOR_INTERVAL: string;
+      MQTT_HOST: string;
+      MQTT_PORT: string;
+      CA_PATH: string;
+      CERT_PATH: string;
+      KEY_PATH: string;
     }
+  }
 }
 
 // If this file has no import/export statements (i.e. is a script)
 // convert it into a module by adding an empty export statement.
-export { }
+export {};
