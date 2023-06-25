8d0ddcb946b4cf17392f9a98155ad1d0f2cada04
Michael Cabanis <mc071@hdm-stuttgart.de>
2023-06-25 12:42:45 +0200

    feat: add thing module

---
+++ infrastructure/thing/main.tf
@@ -0,0 +1,32 @@
+
+resource "aws_iot_thing" "thing" {
+  name = "thing_${var.name}"
+}
+
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "tls_cert_request" "csr" {
+  private_key_pem = tls_private_key.key.private_key_pem
+  subject {
+    organization = "Light Tomorrow"
+    country      = "DE"
+  }
+}
+
+resource "aws_iot_certificate" "cert" {
+  csr    = tls_cert_request.csr.cert_request_pem
+  active = true
+}
+
+resource "aws_iot_policy_attachment" "attachment" {
+  policy = var.policy.name
+  target = aws_iot_certificate.cert.arn
+}
+
+resource "aws_iot_thing_principal_attachment" "attachment" {
+  principal = aws_iot_certificate.cert.arn
+  thing     = aws_iot_thing.thing.name
+}
---
+++ infrastructure/thing/variables.tf
@@ -0,0 +1,7 @@
+variable "name" {
+  description = "The name of the AWS IoT Thing to be created. This name needs to be unique within your AWS account."
+}
+
+variable "policy" {
+  description = "The IoT policy to be attached to the AWS IoT Thing. This defines the permissions for the IoT Thing within AWS IoT."
+}
