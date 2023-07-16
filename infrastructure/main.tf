4eea4a23efdfe0f49b089cef83a233f519a03dd7
Marco de Jesus António <md131@hdm-stuttgart.de>
2023-07-16 12:49:36 +0200

    chore: removed commented code

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -61,38 +61,19 @@ resource "aws_cloudformation_stack" "detector-model-stack" {
   template_body = templatefile("${path.module}/detectormodel/LightActuator.json", { role_arn = aws_iam_role.iotevents_access.arn })
   depends_on    = [time_sleep.wait_30_seconds]
 }
-# TODO: remove comments
-
-# resource "aws_iot_thing_type" "sensor" {
-#   name = "Sensor"
-
-#   properties {
-#     description = "Used for the proximity- and light sensor."
-#   }
-# }
-
-# resource "aws_iot_thing_type" "actuator" {
-#   name = "Actuator"
-
-#   properties {
-#     description = "Used for the workplace light actuators."
-#   }
-# }
 
 module "sensors" {
   for_each = toset(local.sensors)
   source   = "./modules/thing"
   name     = "sensor_${each.key}"
-  #   thing_type = aws_iot_thing_type.sensor
-  policy = aws_iot_policy.thing_policy
+  policy   = aws_iot_policy.thing_policy
 }
 
 module "actuators" {
   for_each = toset(local.actuators)
   source   = "./modules/thing"
   name     = "actuator_${each.key}"
-  #   thing_type = aws_iot_thing_type.actuator
-  policy = aws_iot_policy.thing_policy
+  policy   = aws_iot_policy.thing_policy
 }
 
 data "aws_iam_policy_document" "thing_policy_doc" {
